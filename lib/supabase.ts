75cbefe02e389dc3c7792a0c8aa05aaeae381e42
Sean Goodwin <dealappseo@gmail.com>
2025-12-09 18:43:40 -0800

    Fix: Update Supabase Credentials

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 // HARDCODED - DO NOT USE ENV VARS
 export const supabase = createClient(
     'https://qnnpjhlxljtqyigedwkb.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubnBqaGx4bGp0cXlpZ2Vkd2tiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk1Mjk3NzksImV4cCI6MjAzNTEwNTc3OX0.l2z1L4jK2b3n5m7p9q1r5t7u9w0y2z4x6c8v0b2n4m6p8q0'
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFubnBqaGx4bGp0cXlpZ2Vkd2tiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE5Mzk1OTEsImV4cCI6MjA2NzUxNTU5MX0.6oG2DU_BD1uBnBrDoQFauvN1ZnkKo2ywkuwY-tPaQFw'
 );
